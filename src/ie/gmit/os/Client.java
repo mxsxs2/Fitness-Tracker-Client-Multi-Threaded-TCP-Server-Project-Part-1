6f402af0f5bc07b59756f73f3e154134a711cf4c
Mxsxs2 <>
2018-01-04 21:58:04 +0000

    Implement login and registration

--- src/ie/gmit/os/Client.java
+++ src/ie/gmit/os/Client.java
@@ -5,57 +5,87 @@ import java.net.*;
 import java.util.Scanner;
 
 public class Client {
-	//The server port to send to
-	private int port=2004;
+	// The server port to send to
+	private int port = 2004;
 	ObjectOutputStream out;
 	ObjectInputStream in;
 	String message = "";
+	Scanner stdin;
+
 	/**
-	 * Opens a new socket connection to a specified server and keeps it open until the user says bye 
+	 * Opens a new socket connection to a specified server and keeps it open until
+	 * the user says bye
 	 */
 	public void run() {
-		//Create a new scanner to read from console
-		Scanner stdin = new Scanner(System.in);
+		// Create a new scanner to read from console
+		this.stdin = new Scanner(System.in);
 		System.out.println("Please Enter your IP Address");
-		//Get the ip address to connect to
+		// Get the ip address to connect to
 		String ipaddress = stdin.next();
-		//Try to open the socket
-		try(Socket requestSocket = new Socket(ipaddress, this.port);) {
-			
+		// Try to open the socket
+		try (Socket requestSocket = new Socket(ipaddress, this.port);) {
+
 			System.out.println("Connected to " + ipaddress + " in port 2004");
-			//Create new output stream to socket
+			// Create new output stream to socket
 			out = new ObjectOutputStream(requestSocket.getOutputStream());
-			//Flush the output stream	
+			// Flush the output stream
 			out.flush();
-			//Create new input strem from socket
+			// Create new input strem from socket
 			in = new ObjectInputStream(requestSocket.getInputStream());
-			
-			System.out.println("Hello");
-			//Run until the client says bye
+
+			// Run until the client says bye
 			do {
 				try {
-					//Read a message from the server
+					// Read a message from the server
 					message = (String) in.readObject();
-					//Write out server message
+					// Write out server message
 					System.out.println(message);
-					System.out.println("Please Enter the Message to send...");
-					//Read a message from the client
+					// Read a message from the client
 					message = stdin.next();
-					//Send the message to the server
+					// Send the message to the server
 					sendMessage(message);
+
+					if (message.compareToIgnoreCase("1") == 0) {
+						//PPS number
+						this.doSingleCommunication();
+						//Password
+						this.doSingleCommunication();
+						//Result
+						message = (String) in.readObject();
+						System.out.println(message);
+
+					} else if (message.compareToIgnoreCase("2") == 0) {
+						//PPS Number
+						this.doSingleCommunication();
+						//Password
+						this.doSingleCommunication();
+						//Name
+						this.doSingleCommunication();
+						//Address
+						this.doSingleCommunication();
+						//Age
+						this.doSingleCommunication();
+						//Weight
+						this.doSingleCommunication();
+						//Height
+						this.doSingleCommunication();
+						//Registration result
+						message = (String) in.readObject();
+						System.out.println(message);
+					}
 
 				} catch (ClassNotFoundException classNot) {
 					System.err.println("data received in unknown format");
 				}
-			//Exit if the client says bye
+				// Exit if the client says bye
 			} while (!message.equals("bye"));
 		} catch (UnknownHostException unknownHost) {
-			//Notify the user if the host is not found
+			// Notify the user if the host is not found
 			System.err.println("You are trying to connect to an unknown host!");
 		} catch (IOException ioException) {
 			ioException.printStackTrace();
 		} finally {
-			//Close the streams and the scanner
+			// Close the streams and the scanner
 			try {
 				in.close();
 				out.close();
@@ -68,17 +98,35 @@ public class Client {
 
 	/**
 	 * Send a new message to the client
+	 * 
 	 * @param Message
 	 */
-	void sendMessage(String msg) {
+	private void sendMessage(String msg) {
 		try {
-			//Send the message
+			// Send the message
 			out.writeObject(msg);
-			//Flush the stream
+			// Flush the stream
 			out.flush();
-			System.out.println("client> " + msg);
 		} catch (IOException ioException) {
 			ioException.printStackTrace();
 		}
+	}
+
+	/**
+	 * Reads a message from the server, shows to the user and sends the users
+	 * response to back to the server.
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	private void doSingleCommunication() throws ClassNotFoundException, IOException {
+		// Read message from server
+		message = (String) in.readObject();
+		// Write to console
+		System.out.println(message);
+		// Read message from console
+		message = stdin.next();
+		// Send to server
+		sendMessage(message);
 	}
 }
